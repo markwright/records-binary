1039b3203d7140876f16f2816201e404bdd3fd76
Mark Wright <markwright@internode.on.net>
2011-06-01 14:46:46 +1000

    Add KindStar and Id KindStar Binary intances

--- src/Data/Record/Binary.hs
+++ src/Data/Record/Binary.hs
@@ -8,6 +8,21 @@ import Data.Record
 import Data.Record.Combinators
 import Data.Binary
 
+-- | Serialization of KindStar is unneccessary.
+instance Binary KindStar where
+  -- | put KindStar does not need to write anything.
+  put KindStar = return ()
+  -- | get KindStar just returns KindStar without needing to read anything.
+  get = return KindStar
+
+-- | Serialization of Id KindStar is unneccessary.
+instance Binary KindStar =>
+  Binary (Id KindStar) where
+    -- | put Id KindStar does not need to write anything.
+    put (Id KindStar) = return ()
+    -- | get Id KindStar just returns Id KindStar without needing to read anything.
+    get = return (Id KindStar)
+
 -- | Empty record scheme.  X only has one constructor which has no parameters, hence serialization of X is unneccessary.
 instance Binary style => 
   Binary (X style) where
